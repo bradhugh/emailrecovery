3465a393fd6783c2898089516c10ea384e9b8aa1
Brad Hughes <bradhugh@microsoft.com>
2023-08-18 14:26:30 -0400

    Remove counter slice and components

--- packages/emailrecovery-app/src/app/store.ts
+++ packages/emailrecovery-app/src/app/store.ts
@@ -4,14 +4,12 @@ import {
   Action,
   ThunkDispatch,
 } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import progressReducer from "../features/progress/progressSlice";
 import recoveryReducer from "../features/recovery/recoverySlice";
 import { IServices } from "../services/IServices";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     recovery: recoveryReducer,
     progress: progressReducer,
   },
